edf591d7feb3764468ae424ce23024142cbd6cd2
Tbbas <covernant_4@hotmail.com>
2014-09-09 15:48:27 +0200

    HelloWorld.java
    Källkoden till uppgiften HelloWorld(Labb1)

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*Tobias Alldén och Jonathan Helsing
+ * Grupp 22
+ * 
+ * */
+
+public class HelloWorld {
+/*A program to display the message
+"Hello World!" on standard output
+*/
+	public satic void main(String[] args)	{
+		System.out.println("Hello World!");
+	}
+}//	end	of class HelloWorld
