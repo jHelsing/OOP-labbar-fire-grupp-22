018f7d9c1acbbe85453be94c06cdd9ce9d5e1f23
Tbbas <covernant_4@hotmail.com>
2014-09-22 08:50:53 +0200

    RatNum

---
+++ 22.lab2/Labb2/src/RatNum.java
@@ -0,0 +1,74 @@
+	/* Calculate rational numbers.
+ * @author Tobias Alldén
+ * @author Jonathan Helsing
+ */
+public class RatNum {	
+		private int m;
+		private int n;
+		
+		
+	public RatNum() {
+		this.m = 0;
+		this.n= 1;
+		
+	}
+	
+	public RatNum(int a) {
+		this.m = a;
+		this.n = 1;
+		
+	}
+	
+	public RatNum(int a, int b) {
+		if (b == 0) {
+			throw new NumberFormatException("Denominator = 0");			
+		}
+		else {
+		int devideBy = gcd(a, b);
+		this.m = a/devideBy;
+		this.n = b/devideBy;
+		}
+		
+	}
+	
+	public int getNumerator() {
+		return m;
+	}
+	public int getDenominator() {
+		return n;
+	}
+	
+	
+	
+	
+	public static int gcd(int m, int n) {
+			
+		if ( m<0 || n<0 ) {
+			if (m < 0 && n < 0) {
+				m = (m/-1);
+				n = n/-1;
+			} else if( m < 0 ) {
+				m = m/-1;
+			} else if(n<0) {
+				n = n/-1;
+			}
+			
+		}
+		else if ( m == 0 && n == 0) {
+			throw new IllegalArgumentException();			
+		} else if (n == 0) {
+			return m;			
+		} else {}
+			int r;
+			while (true) {
+				r = m%n;
+				if (r==0) {
+					return n;				
+				}
+				else {
+					m = n;
+					n = r;
+				}
+			}
+	}//end gcd	
+}//end RatNum
