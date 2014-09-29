f360cac6a44b3d240756eeb457d0c00c0af96bc5
Jontehse <jonathan.helsing@gmail.com>
2014-09-29 08:46:31 +0200

    Uppdaterad TestCounter
    Jag har uppdaterat TestCounter så att den är mer utförlig samt
    kommenterat vad jag testar efter.

--- 22.lab3/TestCounter.java
+++ 22.lab3/TestCounter.java
@@ -7,13 +7,18 @@
 public class TestCounter {
 	
 	public static void main(String[] args) {
-		boolean b = true;
+		
 		CounterModel cm1 = new CounterModel();
 		CounterModel cm2 = new CounterModel();
 		System.out.println("cm1 = " + cm1.getValue());
 		System.out.println("cm2 = " + cm2.getValue());
 		
-		for(int i = 0; i < 50; i++) {
+		/*
+		 * Testar så att cm1 och cm2 klarar av att nå positiva och
+		 * negativa värden
+		 */
+		
+		for(int i = 0; i < 100; i++) {
 			cm1.increment();
 			cm2.decrement();
 		}
@@ -21,12 +26,23 @@ public class TestCounter {
 		System.out.println("cm1 = " + cm1.getValue());
 		System.out.println("cm2 = " + cm2.getValue());
 		
-		while(b) {
+		/* 
+		 * Testar så att de klarar av att gå från positiva värden till
+		 * negativa värden och tvärt om.
+		 * cm1 går från pos till neg
+		 * cm2 går från neg till pos
+		*/
+		
+		while(cm1.getValue() != -10) {
 			cm1.decrement();
-			if(cm1.getValue() == -10) {
-				b = false;
-			}
 			System.out.print(cm1.getValue() + " ");
+		}
+		
+		System.out.println("");
+		
+		while(cm2.getValue() != 20) {
+			cm2.increment();
+			System.out.print(cm2.getValue() + " ");
 		}
 	}
 }
