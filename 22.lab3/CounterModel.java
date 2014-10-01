cc31071592f29329c9a53f79369f0a1697d3e3f8
Jontehse <jonathan.helsing@gmail.com>
2014-10-01 15:25:42 +0200

    Första implementationen av Modulus
    Har lagt till modulus för increment(), decrement(), CounterModel() och
    CounterModel(int modulus) så att de räknar med modulus värde när de ska
    öka och minska i värde.

--- 22.lab3/CounterModel.java
+++ 22.lab3/CounterModel.java
@@ -8,18 +8,26 @@ public class CounterModel implements CounterInterface {
 	
 	private int count;
 	private static int nbrOfCounters;
+	private int modulus;
 	
 	public CounterModel() {
 		this.count = 0;
+		this.modulus = 1;
+		nbrOfCounters++;
+	} // end CounterModel
+	
+	public CounterModel(int modulus) {
+		this.count = 0;
+		this.modulus = modulus;
 		nbrOfCounters++;
 	} // end CounterModel
 	
 	public void increment() {
-		this.count++;
+		this.count = count + this.modulus;
 	} // end increment
 	
 	public void decrement() {
-		this.count--;
+		this.count = count - this.modulus;
 	} // end decrement
 	
 	public void reset() {
@@ -32,6 +40,10 @@ public class CounterModel implements CounterInterface {
 	
 	public static int getNbrOfCounters() {
 		return nbrOfCounters;
-	} // getNbrOfCounters
+	} // end getNbrOfCounters
+	
+	public int getModulus() {
+		return this.modulus;
+	} // end getModulus
 	
 }
