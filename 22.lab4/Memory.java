e6a8d92e296fffb5ec1bab59bb3170b99dd37951
Helsing <jonathan.helsing@gmail.com>
2014-10-13 16:42:47 +0200

    Påbörjat Memory.java
    Började jobba på Memory.java men kom inte så långt.

---
+++ 22.lab4/Memory.java
@@ -0,0 +1,21 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *  A class for cards to  be used in Memory.
+ * @author Tobias Allden and Jonathan Helsing 
+ */
+
+public class Memory {
+
+	public static void main(String[] args) {
+		String s = JOptionPane.showInputDialog("Ange antalet rader.");
+		String nbrOfCol = JOptionPane.showInputDialog("Ange antelet kolumner.");
+		String nbrOfRow = JOptionPane.showInputDialog("Ange antalet rader.");
+		int row = Integer.parseInt(nbrOfRow);
+		int col = Integer.parseInt(nbrOfCol);
+		
+	}
+
+}
