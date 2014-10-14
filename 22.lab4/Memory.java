cf55833dcb6d06fd4f09142226190cf9d3a862ec
Tbbas <covernant_4@hotmail.com>
2014-10-14 11:01:32 +0200

    La till lite kod i Memory

--- 22.lab4/Memory.java
+++ 22.lab4/Memory.java
@@ -1,5 +1,6 @@
 
 import java.awt.*;
+
 import javax.swing.*;
 
 /**
@@ -7,15 +8,36 @@ import javax.swing.*;
  * @author Tobias Allden and Jonathan Helsing 
  */
 
-public class Memory {
-
-	public static void main(String[] args) {
-		String s = JOptionPane.showInputDialog("Ange antalet rader.");
-		String nbrOfCol = JOptionPane.showInputDialog("Ange antelet kolumner.");
-		String nbrOfRow = JOptionPane.showInputDialog("Ange antalet rader.");
-		int row = Integer.parseInt(nbrOfRow);
-		int col = Integer.parseInt(nbrOfCol);
+public class Memory extends JFrame {
+	
+	public Memory() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Memory");
+		JMenu menu = new JMenu();
+		JMenuItem newGame = new JMenuItem("Nytt spel");
+		menu.add(newGame);
+		add(menu);
+		pack();
+		setVisible(true);
 		
 	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		String s = JOptionPane.showInputDialog("Ange antalet spelare");
+		//String nbrOfCol = JOptionPane.showInputDialog("Ange antelet kolumner.");
+		//String nbrOfRow = JOptionPane.showInputDialog("Ange antalet rader.");
+		//int row = Integer.parseInt(nbrOfRow);
+		//int col = Integer.parseInt(nbrOfCol);
+		String nbrOfCards = JOptionPane.showInputDialog("Ange antal kort");
+		int numberOfPlayers = Integer.parseInt(s);
+		int numberOfCards = Integer.parseInt(nbrOfCards);
+		Memory game = new Memory();
+		
+	}
+	
+	
 
 }
