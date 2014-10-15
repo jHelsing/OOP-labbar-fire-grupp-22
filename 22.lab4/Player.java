176c59ec9299c65b086fd4988fcd9ee165372a0c
Tbbas <covernant_4@hotmail.com>
2014-10-15 14:20:05 +0200

    Enkel klass för spelare
    Skall i senare skede implementeras i memory

---
+++ 22.lab4/Player.java
@@ -0,0 +1,28 @@
+/* A class for Players to  be used in Memory.
+ * @author Tobias Allden and Jonathan Helsing 
+ * 
+ */
+public class Player{
+	
+	private int score;
+	private String name;
+	
+	public int getScore() {
+		return this.score;
+	}
+	public void setScore() {
+		this.score = this.score+1;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	
+	 	
+	
+	
+}
