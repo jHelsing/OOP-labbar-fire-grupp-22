f48cce9e80b288c8fa566a0d61f954c1ca4060d7
Tbbas <covernant_4@hotmail.com>
2014-10-15 15:44:33 +0200

    Players

--- 22.lab4/Player.java
+++ 22.lab4/Player.java
@@ -7,6 +7,12 @@ public class Player{
 	private int score;
 	private String name;
 	
+	
+	public Player(String name) {
+		this.score = 0;
+		this.name = name;
+	}
+	
 	public int getScore() {
 		return this.score;
 	}
