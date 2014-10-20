0a91bb699242b34b381618aca2bbb6684eccc36e
Tbbas <covernant_4@hotmail.com>
2014-10-20 13:35:25 +0200

    Skrivit om Memory
    Började om på memory, pga. dåligt strukturerad kod, nu fungerar
    utplaceringen samt spelarhanteringen ganska bra, får dock en mysko bugg
    vid kortval, (löser den ikväll förhoppningsvis).
    Skall sticka och gymma nu, jobbar mer på detta ikväll...
    
    Att göra:
    1) Lyssnare för själva spelet
    2) Turordning samt poängräkning
    3) Bugfix
    4) Kommentarer

--- 22.lab4/Memory.java
+++ 22.lab4/Memory.java
@@ -3,9 +3,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.ArrayList;
+
 import javax.annotation.processing.FilerException;
 import javax.swing.*;
+
 
 /**
  *  A class for cards to  be used in Memory.
@@ -13,44 +14,41 @@ import javax.swing.*;
  */
 
 public class Memory extends JFrame {
-	private ArrayList<Player> players = new ArrayList<Player>();
+	private Player[] players;
 	private int rows = 0;
 	private int cols = 0;
 	private GamePanel gamePanel;
-	private JColorfulButton[] cardsButton;
-	
+	private ButtonPanel btnPanel;
+	private PlayersPanel playerPanel;
+	Kort[] cards;
+	Kort[] gameCards;
 	File bildmapp = new File("bildmapp");
-	File[] bilder = this.bildmapp.listFiles();
-	Icon[] img = new ImageIcon[this.bildmapp.listFiles().length];
+	File[] images = this.bildmapp.listFiles();
 
-	public Memory() {
-		for(int i=0; i<this.bilder.length; i++) { // samlar ihop alla bilder
-			this.img[i] = new ImageIcon(bilder[i].getPath());
-		}
-		System.out.println(img.length);
+	public Memory() { //Creates the Memory window, and calls for all methods to initialize the game.
 		startValues();
-		ButtonPanel btnPanel = new ButtonPanel();
-		PlayersPanel playersPanel = new PlayersPanel(players);
+		this.gameCards = new Kort[rows*cols];
+		this.cards = new Kort[images.length];
+		for (int i = 0; i<images.length-1; i++) {
+			this.cards[i] = new Kort(new ImageIcon(images[i].getPath()),Kort.Status.SYNLIGT);
+		}
+		this.btnPanel = new ButtonPanel();
+		add(this.btnPanel,BorderLayout.SOUTH);
+		setPreferredSize(new Dimension(400,300));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Memory");
-		add(btnPanel,BorderLayout.SOUTH);;
-		add(playersPanel,BorderLayout.WEST);
 		nyttSpel();
-		add(gamePanel,BorderLayout.CENTER);
-		
-		pack();
 		setVisible(true);
 	}
 
 
-	private class ButtonPanel extends JPanel implements ActionListener {
+	 class ButtonPanel extends JPanel implements ActionListener {//Creates a Panel with two buttons, newGame and Exit
 		private JButton newButton = new JButton("New Game");
 		private JButton exitButton = new JButton("Exit");
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if (e.getSource().getClass().getName() == "New Game") {
-				
-
+			if (e.getSource() == newButton) {
+				nyttSpel();
 			}
 			else {
 
@@ -68,70 +66,47 @@ public class Memory extends JFrame {
 			add(exitButton);
 		}
 	}
-	private class PlayersPanel extends JPanel{
-		public	PlayersPanel(ArrayList<Player> players) {
-			
+	
+	 private class PlayersPanel extends JPanel{// Creates the panel for Players
+		
+		public	PlayersPanel() {
+			setLayout(new GridLayout(players.length,1));
 			setBackground(Color.RED);
-			//setLayout(new BoxLayout(this,));
+			setPreferredSize(new Dimension(150,150*players.length));
 		}
 	}
+	
+	
+		
+	
+	
+	
 
 	private class GamePanel extends JPanel implements ActionListener {
+		Kort tmpKort1 = null;
+		Kort tmpKort2 = null; 
 		
 		public GamePanel() {
-			setLayout(new GridLayout(cols,rows));
-			setBackground(Color.blue);
+			setLayout(new GridLayout(rows,cols));
 		}
 
+			
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			System.out.println("hi!");
-			
 		}
 	}
+
 	
-	private void nyttSpel() { // initierar spel med kort.
-		this.gamePanel = new GamePanel();
-		
-		Kort[] gameCards = new Kort[this.rows*this.cols];
-		for(int i=0; i<gameCards.length; i++) {
-			gameCards[i] = new Kort(this.img[i]);
-		}
-		
-	
-		for(int i=0; i<gameCards.length; i++) {
-			cardsButton[i] = new JColorfulButton(gameCards[i].getIcon());
-			cardsButton[i].setBackground(Color.BLUE);
-			cardsButton[i].setSize(50, 50);
-			gamePanel.add(cardsButton[i]);
-		}
-		
-		
-		/*
-		this.gamePanel = new GamePanel();
-		//Kort[] cards = new Kort[rows*cols];
-		ArrayList<Kort> cards = new ArrayList<Kort>();
-		System.out.println(bilder[0].getPath());
-		for(int i = 0;i<bilder.length;i++) {
-			String path = bilder[i].getPath();
-			cards.add(new Kort(new ImageIcon(path)));
-			cards.setIcon(new ImageIcon(path));
-		}
-		this.gamePanel.add(cards);
-		*/
-	}
-	
-	private void startValues() { // Kolla input om korrekt, samt sï¿½tt startvï¿½rden
+	private void startValues() { //Check if valid input and create startvalues
 		boolean valuesSet = false;
 		while (valuesSet == false) {
 			try {
 				this.cols = Integer.parseInt(JOptionPane.showInputDialog("How many columns?"));				 
-				this.rows = Integer.parseInt(JOptionPane.showInputDialog("How many rows?"));				
-				if((this.cols*this.rows)/2 > bilder.length) {
-					throw new FilerException("Fï¿½r stor spelplan fï¿½r antalet kort");
-				} //else if(this.cols%2 != 0) {
-					//throw new IllegalArgumentException("MÃ¥ste ha ett jÃ¤mnt antal rutor");
-				//}
+				this.rows = Integer.parseInt(JOptionPane.showInputDialog("How many rows?"));
+				valuesSet = true;				
+				if((this.cols*this.rows)/2> images.length) {
+					throw new FilerException("För stor spelplan för antalet kort");
+				}
 				valuesSet = true;
 				}
 				catch(NumberFormatException e) {
@@ -140,22 +115,60 @@ public class Memory extends JFrame {
 				catch(FilerException e) {
 					JOptionPane.showMessageDialog(null, e.getMessage(),"Fel", JOptionPane.ERROR_MESSAGE);
 				}
-				catch(IllegalArgumentException e) {
-					JOptionPane.showMessageDialog(null, e.getMessage(), "Fel", JOptionPane.ERROR_MESSAGE);
+			}
+
+			}
+
+	private void nyttSpel() {
+		if (this.players == null) {
+			int num = 0;
+			while(num == 0) {
+				String inputPlayers = JOptionPane.showInputDialog("how many players");
+				try {
+					num = Integer.parseInt(inputPlayers);
+					this.players = new Player[num];
+					for(int i= 0; i<players.length;i++) {
+						players[i] = new Player(Integer.toString(i));						
+					}
+					this.playerPanel = new PlayersPanel();
+					for(int i = 0; i<players.length;i++) {
+						this.playerPanel.add(players[i]);
+					}
+					add(this.playerPanel,BorderLayout.WEST);
+				}
+				catch(NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "Ange ett nummer","Fel", JOptionPane.ERROR_MESSAGE);
 				}
 			}
-
+			
+		}
+		else{
+			for(int i = 0; i<this.players.length;i++) {
+				this.players[i].resetScore();
 			}
-
-
-
-
-
-
-
-
-
-
+				
+		}
+		
+		Verktyg.slumpOrdning(cards);
+		for(int i = 0; i<this.gameCards.length/2;i++) {
+			this.gameCards[i] = cards[i];
+			System.out.println("Antal kort:" + gameCards.length);
+			this.gameCards[i+(this.gameCards.length/2)] = this.gameCards[i].copy(); 
+		}
+		Verktyg.slumpOrdning(this.gameCards);
+		System.out.println("Antal kort:" + gameCards.length);
+		this.gamePanel = new GamePanel();
+		for(int i = 0; i<gameCards.length;i++) {
+			gamePanel.add(this.gameCards[i]);
+		}
+		add(gamePanel,BorderLayout.CENTER);
+		pack();
+		invalidate();
+		validate();
+		
+		
+		
+	}
 	public static void main(String[] args) {
 		Memory game = new Memory();
 	}
